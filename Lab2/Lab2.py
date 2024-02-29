1170cbb9a27e23c8131ad3a15bc2ab71fde881e5
Berco Andrei <andrei2003berco@gmail.com>
2024-02-29 12:22:04 +0200

    add method to show grammar and chec the FA for NFA or DFA

--- Lab2/Lab2.py
+++ Lab2/Lab2.py
@@ -1,33 +1,47 @@
+from collections import Counter
+
 class FiniteAutomatom:
     def __init__(self):
         self.Q = ['q0','q1','q2','q3','q4']
         self.Sigma = ['a','b']
         self.Delta = {
-            ('q0', 'a') : 'q1',
-            ('q1', 'b') : 'q1',
-            ('q1', 'a') : 'q2',
-            ('q2', 'b') : 'q2',
-            ('q2', 'b') : 'q3',
-            ('q3', 'b') : 'q4',
-            ('q3', 'a') : 'q1'
+            ('q0', 'a') : ['q1'],
+            ('q1', 'b') : ['q1'],
+            ('q1', 'a') : ['q2'],
+            ('q2', 'b') : ['q2', 'q3'],
+            ('q3', 'b') : ['q4'],
+            ('q3', 'a') : ['q1']
         }
         self.q0 = 'q0'
-        self.F = 'q4'
+        self.F = ['q4']
 
     def convert_to_grammar(self):
-        S = q[0]
+        S = self.Q[0]
         V_n = self.Q
         V_t = self.Sigma
         P = []
-        for state in self.S:
+        for state in self.Q:
             for symbol in self.Sigma:
                 if (state, symbol) in self.Delta:
-                    next_state = self.Delta[(state, symbol)]
-                    P.append((state, symbol, next_state))
+                    if len(self.Delta[(state, symbol)]) > 1:
+                        for i in range(len(self.Delta[(state, symbol)])):
+                            next_state = self.Delta[(state, symbol)][i]
+                            P.append((state, symbol, next_state))
+                    else:
+                        next_state = self.Delta[(state, symbol)][0]
+                        P.append((state, symbol, next_state))
         for final_state in self.F:
             P.append((final_state, '', 'e'))
         return Grammar(S, V_n, V_t, P)
 
+    def checkDeterministic(self):
+        counter = {}
+        for key, value in self.Delta.items():
+            if(len(value))>1:
+                return False
+        return True
+
+         
 
 class Grammar:
     def __init__(self, S, V_n, V_t, P):
@@ -36,10 +50,21 @@ class Grammar:
         self.V_t  = V_t
         self.P = P
 
-    def show_gramamr():
-        print("V_n = {", ', '.join(map(str, V_n)), '}' )
+    def show_gramamr(self):
+        print("VN = {", ', '.join(map(str, self.V_n)), '}' )
+        print("VT = {", ', '.join(map(str, self.V_t)), '}' )
+        print("P = { ")
+        for el in self.P:
+            a,b,c = el
+            print(f"    {a} -> {b}{c}")
+        print("}")
 
-a= [1,2,3,4]
-print("V_n = {", ', '.join(map(str, a)), '}')    
-        
-              
+finiteAutomatom = FiniteAutomatom()
+grammar = finiteAutomatom.convert_to_grammar()
+grammar.show_gramamr()
+
+if finiteAutomatom.checkDeterministic()==False:
+    print('Non-Deterministic Automatom')
+else:
+    print('Deterministic Automatom')
+    
