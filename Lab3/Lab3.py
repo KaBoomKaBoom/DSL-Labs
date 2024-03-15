94278ce8a1a6d658c8badf9c7a6a6efbc33ca3d4
Berco Andrei <andrei2003berco@gmail.com>
2024-03-15 15:28:07 +0200

    add a lexer for complex expressions

--- Lab3/Lab3.py
+++ Lab3/Lab3.py
@@ -1,6 +1,7 @@
 import re
 
 # Token types
+IDENTIFIER = 'IDENTIFIER'
 INTEGER = 'INTEGER'
 CHAR = 'CHAR'
 STRING = 'STRING'
@@ -8,26 +9,51 @@ PLUS = 'PLUS'
 MINUS = 'MINUS'
 MULTIPLY = 'MULTIPLY'
 DIVIDE = 'DIVIDE'
-EQUAL = 'EQUAL'
+ASSIGN = 'ASSIGN'
 LPAREN = 'LPAREN'
 RPAREN = 'RPAREN'
+EQUAL = 'EQUAL'
+LESS = 'LESS'
+GREATER = 'GREATER'
+LESS_EQUAL = 'LESS_EQUAL'
+GREATER_EQUAL = 'GREATER_EQUAL'
+FOR = 'FOR'
+IN = 'IN'
+RANGE = 'RANGE'
+IF = 'IF'
+ELSE = 'ELSE'
+PRINT= 'PRINT'
+COLON = 'COLON'
+INCREMENT = 'INCREMENT'
 SPACE = 'SPACE'
+NEWLINE = 'NEWLINE'
 EOF = 'EOF'
 
 # Regular expressions for token patterns
 TOKEN_REGEX = [
+    (r'[a-zA-Z][a-zA-Z0-9_]*', IDENTIFIER),
     (r'\d+', INTEGER),
-    (r'[a-zA-Z][a-zA-Z]+', STRING),
-    (r'[a-zA-Z]', CHAR),
+    (r'\'[a-zA-Z\s][a-zA-Z\s]+\'', STRING),
+    (r'\'[a-zA-Z]\'', CHAR),
+    (r'\+=', INCREMENT),
     (r'\+', PLUS),
     (r'-', MINUS),
     (r'\*', MULTIPLY),
     (r'/', DIVIDE),
     (r'\(', LPAREN),
     (r'\)', RPAREN),
-    (r'=', EQUAL),
-    (r'\s+', SPACE)
+    (r'==', EQUAL),
+    (r'<=', LESS_EQUAL),
+    (r'>=', GREATER_EQUAL),
+    (r'<', LESS),
+    (r'>', GREATER),
+    (r'=', ASSIGN),
+    (r':', COLON),
+    (r'\s+', SPACE),
+    (r'\n', NEWLINE)
 ]
+
+KEY_WORDS = ['for', 'in', 'range', 'if', 'else', 'print', 'while' ]
 
 class Token:
     def __init__(self, type, value):
@@ -46,39 +72,31 @@ class Lexer:
         raise Exception('Invalid character')
 
     def get_next_token(self):
-        test = self.text.split(' ')
-        if self.pos >= len(test):
-            return Token(EOF, None)
-
-        # for pattern, token_type in TOKEN_REGEX:
-        #     regex = re.compile(pattern)
-        #     match = regex.match(text)
-        #     if match:
-        #         value = match.group(0)
-        #         self.pos += len(value)
-        #         return Token(token_type, value)
+        while self.pos < len(self.text):
+            for pattern, token_type in TOKEN_REGEX:
+                regex = re.compile(pattern)
+                match = regex.match(self.text, self.pos)
+                if match:
+                    value = match.group(0)
+                    self.pos = match.end()
+                    if token_type == SPACE or token_type == NEWLINE:
+                        break  # Skip spaces
+                    elif value in KEY_WORDS:
+                        return Token(value.upper(), value)
+                    else:
+                        return Token(token_type, value)
+            else:
+                self.error()
         
-        el = test[self.pos]
-        print(el)
-        for pattern, token_type in TOKEN_REGEX:
-            regex = re.compile(pattern)
-            match = regex.match(el)
-            if match:
-                self.pos += 1
-                return Token(token_type, el)
-            
-            
-
-        self.error()
-
-
+        return Token(EOF, None)
 
 if __name__ == '__main__':
-    lexer = Lexer("te = 3 + 5 * ( 10 - 2 ) / 4")
+    #lexer = Lexer("i = 5\n for i in range(10): print(i)\n i +=1\n if i == 10: print(Hello)\n else: print(World)")
+    #lexer = Lexer("i = 0\n while i < 10: print(i)\n i += 1\n if i == 10: print(Hello)\n else: print(World)\n")
+    lexer = Lexer("age = 18\n if age >= 18: print('You are an adult')\n else: print('You are a child')\n")
     while True:
         token = lexer.get_next_token()
         if token.type == EOF:
             break
         print(token)
-    lexer = "test = 3 + 5 * ( 10 - 2 ) / 4"
-    #print(lexer.split(' '))
+
