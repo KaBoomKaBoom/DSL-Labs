3ff8d11447e15c19617f8f1390f0b5160f03e371
Berco Andrei <andrei2003berco@gmail.com>
2024-03-15 14:20:45 +0200

    add a lexer for simple expressions

--- Lab3/Lab3.py
+++ Lab3/Lab3.py
@@ -2,23 +2,31 @@ import re
 
 # Token types
 INTEGER = 'INTEGER'
+CHAR = 'CHAR'
+STRING = 'STRING'
 PLUS = 'PLUS'
 MINUS = 'MINUS'
 MULTIPLY = 'MULTIPLY'
 DIVIDE = 'DIVIDE'
+EQUAL = 'EQUAL'
 LPAREN = 'LPAREN'
 RPAREN = 'RPAREN'
+SPACE = 'SPACE'
 EOF = 'EOF'
 
 # Regular expressions for token patterns
 TOKEN_REGEX = [
     (r'\d+', INTEGER),
+    (r'[a-zA-Z][a-zA-Z]+', STRING),
+    (r'[a-zA-Z]', CHAR),
     (r'\+', PLUS),
     (r'-', MINUS),
     (r'\*', MULTIPLY),
     (r'/', DIVIDE),
     (r'\(', LPAREN),
     (r'\)', RPAREN),
+    (r'=', EQUAL),
+    (r'\s+', SPACE)
 ]
 
 class Token:
@@ -38,25 +46,39 @@ class Lexer:
         raise Exception('Invalid character')
 
     def get_next_token(self):
-        if self.pos >= len(self.text):
+        test = self.text.split(' ')
+        if self.pos >= len(test):
             return Token(EOF, None)
 
-        text = self.text[self.pos:]
-
+        # for pattern, token_type in TOKEN_REGEX:
+        #     regex = re.compile(pattern)
+        #     match = regex.match(text)
+        #     if match:
+        #         value = match.group(0)
+        #         self.pos += len(value)
+        #         return Token(token_type, value)
+        
+        el = test[self.pos]
+        print(el)
         for pattern, token_type in TOKEN_REGEX:
             regex = re.compile(pattern)
-            match = regex.match(text)
+            match = regex.match(el)
             if match:
-                value = match.group(0)
-                self.pos += len(value)
-                return Token(token_type, value)
+                self.pos += 1
+                return Token(token_type, el)
+            
+            
 
         self.error()
 
+
+
 if __name__ == '__main__':
-    lexer = Lexer("3 + 5 * ( 10 - 2 ) / 4")
+    lexer = Lexer("te = 3 + 5 * ( 10 - 2 ) / 4")
     while True:
         token = lexer.get_next_token()
         if token.type == EOF:
             break
         print(token)
+    lexer = "test = 3 + 5 * ( 10 - 2 ) / 4"
+    #print(lexer.split(' '))
