c6fc511975602aee93cc6d0a7a5aa6185794afc5
Berco Andrei <andrei2003berco@gmail.com>
2024-03-29 17:18:37 +0200

    add almost functional string generator (except fixed amount of chars)

--- Lab4/Lab4.py
+++ Lab4/Lab4.py
@@ -1,6 +1,65 @@
-import re
+import random
 
+def generateString(rule):
 
-regex = "(a|b)(c|d)E+G?"
-tokens = re.split("/", regex)
-print(list(regex))
+    string=""
+    i = 0
+    while i < len(rule):
+
+        #cover case when 1 or more occurrences from options
+        if rule[i] == "(" and rule[i + 1] == "+":
+
+            for _ in range(random.randint(1, 5)):
+                string += choice(options(rule[i+1:rule.index(")", i)])) 
+            i = rule.index(")", i) + 1
+
+        #cover case when 0 or more occurrences from options
+        elif rule[i] == "(" and rule[i + 1] == "*":
+
+            for _ in range(random.randint(0, 5)):
+                string += choice(options(rule[i+1:rule.index(")", i)])) 
+            i = rule.index(")", i) + 1
+        
+        #cover case when fixed occurrences from options
+        elif rule[i] == "(" and rule.index(")", i) + 1 == "{":
+            for i in range(int(rule.index("}", i)) - 1):
+                string += choice(options(rule[i+1:rule.index(")", i)]))
+            i = rule.index("}", i) + 1  
+        
+        #cover case when 0 or 1 occurrence from options
+        elif rule[i] == "(" and rule.index(")", i) + 1 == "?" :
+            if random.randint(0, 1):
+                string += choice(options(rule[i+1:rule.index(")", i)])) 
+            i = rule.index(")", i) + 1
+
+        elif  i < len(rule) - 2 and rule[i + 1] == "?":
+            if random.randint(0, 1):
+                string += rule[i]
+            i += 2
+
+        #cover case when 1 occurrence from options
+        elif rule[i] == "(":
+            string += choice(options(rule[i+1:rule.index(")", i)])) 
+            i = rule.index(")", i) + 1 
+            
+
+        elif rule[i] in ["|", "(", ")", "+", "*", "?", "{", "}"]:
+            i += 1
+            pass
+
+        else:
+            string += rule[i]
+            i += 1
+        
+        print(i,'    ', string)
+    return string
+
+def choice(options):
+    return random.choice(options)
+
+def options(sequence):
+    return sequence.split("|")
+
+rule1 = "O(P|Q|R)+2?(3|4){" + "2}"
+print(rule1)
+print(generateString(rule1))
